fc49c5e3a5f82f371d7e0ffc1ca8f738bce69537
Mogilnik Valeria <54841241+Variarmo@users.noreply.github.com>
2025-01-10 14:58:42 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,388 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="fonts.css">
+    <link rel="stylesheet" href="https://yookassa.ru/integration/simplepay/css/yookassa_construct_form.css">
+    <script src="https://yookassa.ru/integration/simplepay/js/yookassa_construct_form.js"></script>
+    <title>Энергия красоты в макияже</title>
+</head>
+<body>
+<div class="header flexBlock imgLeft">
+    <img src="./img/pic12.jpg">
+    <div class="textBlockRight">
+        <h1>Энергия красоты в&nbsp;макияже</h1>
+        <p class="subtitle">Идеальный макияж, который скрывает недостатки и подчеркивает достоинства, поможет влюбится в себя и придаст уверенности за 20-30 минут в день</p>
+        <a href="#tariffs" class="btnToTariffs">Записаться на курс</a>
+    </div>
+</div>
+
+<div class="aboutVeroniikamo flexBlock imgRight">
+  <div class="textBlockLeft">
+      <h2>Создательница курса<br>Вероника</h2>
+        <ul>
+            <li>Провела около 150 занятий по макияжу для себя</li>
+            <li>Девушки, которые постоянно применяют полученные на занятиях навыки делают макияж так, будто сходили к визажисту</li>
+            <li>Я делаю себе макияж каждый день и каждый день его трансформирую</li>
+            <li>Я имею 7 летний опыт в сфере макияжа и отлично разбираюсь в многообразии косметики и кистей</li>
+            <li>Слежу за трендами и проходила обучения у топов индустрии: Пони Рома, Виктория Гришина, Валерия Пиминова, Яна Панфиловская и др</li>
+        </ul>
+  </div>
+    <img src="./img/pic5.jpg"/>
+</div>
+
+<div class="for flexBlock imgLeft">
+    <img class="desktop" src="./img/pic13.JPG">
+    <div class="textBlockRight">
+        <h2>Для кого этот курс?</h2>
+        <ul>
+            <li>Для девушек, которые хотят делать свежий макияж каждый день</li>
+            <li>Для экспертов, которые ведут свой блог и им важно выглядеть дорого и изыскано</li>
+            <li>Для мам в декрете, которые хотят быть красивыми в короткие сроки</li>
+            <li>Для работниц найма, которым необходимо выглядеть презентабельно и ухоженно</li>
+            <li>Для фрилансеров, которым важно делать правильный макияж, чтобы хорошо выглядеть</li>
+        </ul>
+    </div>
+</div>
+
+<div class="course flexBlock imgRight">
+    <img class="mobile" src="./img/pic141.JPG">
+    <div class="textBlockLeft">
+        <h2>Зачем тебе этот курс?</h2>
+        <ul>
+            <li>Ты сможешь подбирать себе кисти и косметику, не выбрасывая деньги на ветер</li>
+            <li>Будешь знать что, как и куда наносить</li>
+            <li>Сможешь рисовать стрелки даже если у тебя нависшее веко</li>
+            <li>Научишься увеличивать губы без уколов</li>
+            <li>Ты сможешь получать удовольствие в процессе макияжа, чувствовать себя увереннее, делать селфи и влюбляться в себя</li>
+            <li>Погрузишься в благоприятные для наработки опыта условия</li>
+            <li>Получишь поддерживающее женское окружение, с которым будете осваивать новую информацию</li>
+        </ul>
+    </div>
+    <img class="desktop" src="./img/pic14.JPG">
+</div>
+
+<div class="now container">
+    <div class="text">
+        <h2>Как дела сейчас?</h2>
+        <ul>
+            <li>Не умение подбирать уход</li>
+            <li>Непонимание как работать с лицом</li>
+            <li>Тональное средство ложится пятнами</li>
+            <li>Сложности с контурингом</li>
+            <li>Макияж глаз скатывается</li>
+            <li>Не получаются стрелки</li>
+            <li>Не хватает времени на макияж</li>
+            <li>Знания, которые есть устарели или ошибочны</li>
+        </ul>
+    </div>
+    <img class="img1 desktop" src="./img/pic7.jpg">
+    <img class="img2 desktop" src="./img/pic11.jpg">
+
+    <h2>Почему так?</h2>
+    <ul>
+        <li>Нет понимания в косметических продуктах и кистях.</li>
+        <li>Нет системности, спешка.</li>
+        <li>Нет мотивации и поддержки, чтобы не сдаваться и отработать навык</li>
+    </ul>
+    <img class="mobile" src="./img/pic21.PNG">
+</div>
+
+<div class="what">
+    <div class="container">
+        <h2>Что мы сделаем, чтобы твой макияж был безупречным?</h2>
+        <ul>
+            <li>Подберем грамотный уход с поддержкой косметолога</li>
+            <li>Разберем твою косметичку и определим какие продукты можно оставить, а какие докупить</li>
+            <li>Изучим кисти: формы, ворс и функции. Подберем набор для себя</li>
+            <li>Закрепим навык 3-ех основных техник макияжа</li>
+            <li>Научимся рисовать стрелки</li>
+            <li>Погрузимся в обучение в хорошей компании</li>
+            <li>Разберемся, как с помощью макияжа получать ресурс</li>
+        </ul>
+    </div>
+</div>
+
+<div class="skills">
+    <img  class="desktop" src="./img/pic18.JPG">
+    <img  class="mobile" src="./img/pic181.png">
+    <div class="container">
+        <h2>Что вы получите на курсе?</h2>
+        <ul>
+            <li class="block">
+                <h4>Блок 1. Собираем косметичку</h4>
+                <ul>
+                    <li>Подобрать уходовую косметику</li>
+                    <li>Разобрать свою косметичку и подобрать недостающие средства</li>
+                    <li>Собрать набор кистей</li>
+                </ul>
+            </li>
+            <li class="block">
+                <h4>Блок 2. Нюдовый макияж</h4>
+                <ul>
+                    <li>Тонирование</li>
+                    <li>Коррекция лица</li>
+                    <li>Оформление бровей</li>
+                    <li>Нюдовый макияж глаз и губ</li>
+                </ul>
+            </li>
+            <li class="block">
+                <h4>Блок 3. Смоки айс</h4>
+                <ul>
+                    <li>Делать коррекцию лица с помощью скульптора, хайлайтера и румян</li>
+                    <li>Создавать красивый макияж для разных случаев: от повседневного до вечернего</li>
+                    <li>Делать красивые селфи</li>
+                </ul>
+            </li>
+            <li class="block">
+                <h4>Блок 4. Стрелки</h4>
+                <ul>
+                    <li>Создание мягкой стрелки карандашом</li>
+                    <li>Рисование стрелок маркером</li>
+                </ul>
+            </li>
+            <li class="block">
+                <h4>А также:</h4>
+                <ul>
+                    <li>2 групповых созвона</li>
+                    <li>Лекция от косметолога по уходу</li>
+                    <li>Лекция от стилиста</li>
+                    <li>Видео урок от мастера по прическам по локонам</li>
+                    <li>Ресурсные задания</li>
+                    <li>Радость, уверенность, вдохновение</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+    <div class="argument container">
+        <h3>Важный аргумент</h3>
+        <p>Вы сможете научиться делать себе макияж онлайн, потому что я создам условия и повод для отработки и помогу вам научиться применять полученные знания</p>
+    </div>
+</div>
+
+<div class="product flexBlock imgLeft">
+    <img src="./img/pic20.jpg">
+    <div class="textBlockRight">
+        <h2>Что входит в продукт</h2>
+        <ul>
+            <li>Предобучение, где вы подберете себе уход, а также косметику и кисти для макияжа</li>
+            <li>Мотивация на систематическую отработку</li>
+            <li>Лекции от приглашенных спикеров</li>
+            <li>Розыгрыш подарков среди лучших учеников курса</li>
+            <li>Разбор ошибок на эфирах</li>
+            <li>Возможность укрепить уверенность в себе</li>
+        </ul>
+    </div>
+</div>
+
+<div id="tariffs">
+    <h2>Тарифы</h2>
+    <div class="tariffs_wrap">
+        <div class="tariff_item">
+            <div class="tariff_item-title">
+                <h4>Групповой</h4>
+            </div>
+            <ul class="tariff_item-options">
+                <li>5 обучающих блоков</li>
+                <li>2 групповых созвона</li>
+                <li>Групповой чат с обратной связью</li>
+                <li>Бонусные эфиры за выполнение заданий</li>
+                <li>Бонусные материалы</li>
+                <li>Возможность участия в розыгрыше за выполнение всех заданий</li>
+            </ul>
+            <div class="tariff_item-price">
+                <span class="tariff_item-old-price">8 000</span>
+                <span class="tariff_item-new-price">7 000 рублей</span>
+            </div>
+            <a class="pay-button" href="#payS">Выбрать тариф</a>
+            <div class="places">Всего 5 мест</div>
+        </div>
+        <div class="tariff_item">
+            <div class="tariff_item-title">
+                <h4>Групповой</h4>
+                <p  class="plus">подбор ухода от косметолога</p>
+            </div>
+            <ul class="tariff_item-options">
+                <li>5 обучающих блоков</li>
+                <li>2 групповых созвона</li>
+                <li>Групповой чат с обратной связью</li>
+                <li>Бонусные эфиры за выполнение заданий</li>
+                <li>Бонусные материалы</li>
+                <li>Возможность участия в розыгрыше за выполнение всех заданий</li>
+                <li>Индивидуальный подбор ухода за кожей лица с рекомендациями от косметолога</li>
+            </ul>
+            <div class="tariff_item-price">
+                <span class="tariff_item-old-price">10 000</span>
+                <span class="tariff_item-new-price">9 000 рублей</span>
+            </div>
+            <a class="pay-button" href="#payM">Выбрать тариф</a>
+            <div class="places">Всего 5 мест</div>
+        </div>
+        <div class="tariff_item">
+            <div class="tariff_item-title">
+                <h4>Индивидуальный формат с Вероникой</h4>
+                <p  class="plus">подбор ухода от косметолога</p>
+            </div>
+            <ul class="tariff_item-options">
+                <li>5 обучающих блоков</li>
+                <li>2 групповых созвона</li>
+                <li>Индивидуальная обратная связь</li>
+                <li>Бонусные эфиры за выполнение заданий</li>
+                <li>Бонусные материалы</li>
+                <li>Возможность участия в розыгрыше за выполнение всех заданий</li>
+                <li>Индивидуальный подбор ухода за кожей лица с рекомендациями от косметолога</li>
+            </ul>
+            <div class="tariff_item-price">
+                <span class="tariff_item-old-price">17 000</span>
+                <span class="tariff_item-new-price">15 000 рублей</span>
+            </div>
+            <a class="pay-button" href="#payL">Выбрать тариф</a>
+            <div class="places">Всего 3 места</div>
+        </div>
+    </div>
+</div>
+
+<div id="payS" class="modal-pay">
+    <div class="modal-back"></div>
+    <div class="modalClose"></div>
+    <form class="yoomoney-payment-form" action="https://yookassa.ru/integration/simplepay/payment" method="post" accept-charset="utf-8" >
+        <div class="ym-products">
+            <div class="ym-block-title ym-products-title">Тариф «Групповой»</div>
+            <div class="ym-product">
+                <div class="ym-product-line">
+                    <span class="ym-product-price" data-price="7000" data-id="574" data-count="1">7&nbsp;000,00&nbsp;₽</span>
+                </div>
+
+                <input disabled="" type="hidden" name="text" value="Тариф «Групповой»">
+                <input disabled="" type="hidden" name="price" value="7000">
+                <input disabled="" type="hidden" name="quantity" value="1">
+                <input disabled="" type="hidden" name="paymentSubjectType" value="service">
+                <input disabled="" type="hidden" name="paymentMethodType" value="full_prepayment">
+                <input disabled="" type="hidden" name="tax" value="1">
+            </div>
+        </div>
+        <input value="" type="hidden" name="ym_merchant_receipt">
+        <div class="ym-customer-info">
+            <div class="ym-block-title">О покупателе</div>
+            <input name="cps_email" class="ym-input" placeholder="Email" type="text" value="">
+            <input name="cps_phone" class="ym-input" placeholder="Телефон" type="text" value="">
+        </div>
+        <div class="ym-hidden-inputs">
+        </div>
+        <input name="customerNumber" type="hidden" value="Оплата курса Энергия красоты в макияже">
+        <div class="ym-payment-btn-block ym-before-line ym-align-space-between">
+            <div class="ym-input-icon-rub ym-display-none">
+                <input name="sum" placeholder="0.00" class="ym-input ym-sum-input ym-required-input" type="number" step="any" value="7000">
+            </div>
+            <button data-text="Оплатить" class="ym-btn-pay ym-result-price"><span class="ym-text-crop">Оплатить</span></button>
+            <lineargradient id="paint0_linear" x1="57.3564" y1="0" x2="57.3564" y2="63" gradientunits="userSpaceOnUse">
+                <stop stop-color="#0160D1"><stop offset="1" stop-color="#00479C"></stop></stop>
+            </lineargradient>
+            <lineargradient id="paint1_linear" x1="12.8472" y1="9.35889" x2="12.8472" y2="55.2393" gradientunits="userSpaceOnUse">
+                <stop stop-color="#0160D1"><stop offset="1" stop-color="#00479C"></stop></stop>
+            </lineargradient>
+        </div>
+        <input name="shopId" type="hidden" value="957982">
+    </form>
+</div>
+<div id="payM"  class="modal-pay">
+    <div class="modal-back"></div>
+    <div class="modalClose"></div>
+    <form class="yoomoney-payment-form" action="https://yookassa.ru/integration/simplepay/payment" method="post" accept-charset="utf-8" >
+        <div class="ym-products">
+            <div class="ym-block-title ym-products-title">Тариф «Групповой + подбор ухода от косметолога»</div>
+            <div class="ym-product">
+                <div class="ym-product-line">
+                    <span class="ym-product-price" data-price="9000" data-id="574" data-count="1">9&nbsp;000,00&nbsp;₽</span>
+                </div>
+
+                <input disabled="" type="hidden" name="text" value="Тариф «Групповой + подбор ухода от косметолога»">
+                <input disabled="" type="hidden" name="price" value="9000">
+                <input disabled="" type="hidden" name="quantity" value="1">
+                <input disabled="" type="hidden" name="paymentSubjectType" value="service">
+                <input disabled="" type="hidden" name="paymentMethodType" value="full_prepayment">
+                <input disabled="" type="hidden" name="tax" value="1">
+            </div>
+        </div>
+        <input value="" type="hidden" name="ym_merchant_receipt">
+        <div class="ym-customer-info">
+            <div class="ym-block-title">О покупателе</div>
+            <input name="cps_email" class="ym-input" placeholder="Email" type="text" value="">
+            <input name="cps_phone" class="ym-input" placeholder="Телефон" type="text" value="">
+        </div>
+        <div class="ym-hidden-inputs">
+        </div>
+        <input name="customerNumber" type="hidden" value="Оплата курса Энергия красоты в макияже">
+        <div class="ym-payment-btn-block ym-before-line ym-align-space-between">
+            <div class="ym-input-icon-rub ym-display-none">
+                <input name="sum" placeholder="0.00" class="ym-input ym-sum-input ym-required-input" type="number" step="any" value="9000">
+            </div>
+            <button data-text="Оплатить" class="ym-btn-pay ym-result-price"><span class="ym-text-crop">Оплатить</span></button>
+            <lineargradient id="paint0_linear" x1="57.3564" y1="0" x2="57.3564" y2="63" gradientunits="userSpaceOnUse"><stop stop-color="#0160D1"><stop offset="1" stop-color="#00479C"></stop></stop></lineargradient>
+            <lineargradient id="paint1_linear" x1="12.8472" y1="9.35889" x2="12.8472" y2="55.2393" gradientunits="userSpaceOnUse"><stop stop-color="#0160D1"><stop offset="1" stop-color="#00479C"></stop></stop></lineargradient>
+        </div>
+        <input name="shopId" type="hidden" value="957982">
+    </form>
+</div>
+<div id="payL" class="modal-pay">
+    <div class="modalClose"></div>
+    <div class="modal-back"></div>
+    <form class="yoomoney-payment-form" action="https://yookassa.ru/integration/simplepay/payment" method="post" accept-charset="utf-8" >
+        <div class="ym-products">
+            <div class="ym-block-title ym-products-title">Тариф «Индивидуальный формат с Вероникой + подбор ухода от косметолога»</div>
+            <div class="ym-product">
+                <div class="ym-product-line">
+                    <span class="ym-product-price" data-price="15000" data-id="574" data-count="1">15&nbsp;000,00&nbsp;₽</span>
+                </div>
+
+                <input disabled="" type="hidden" name="text" value="Тариф «Индивидуальный формат с Вероникой + подбор ухода от косметолога»">
+                <input disabled="" type="hidden" name="price" value="15000">
+                <input disabled="" type="hidden" name="quantity" value="1">
+                <input disabled="" type="hidden" name="paymentSubjectType" value="service">
+                <input disabled="" type="hidden" name="paymentMethodType" value="full_prepayment">
+                <input disabled="" type="hidden" name="tax" value="1">
+            </div>
+        </div>
+        <input value="" type="hidden" name="ym_merchant_receipt">
+        <div class="ym-customer-info">
+            <div class="ym-block-title">О покупателе</div>
+            <input name="cps_email" class="ym-input" placeholder="Email" type="text" value="">
+            <input name="cps_phone" class="ym-input" placeholder="Телефон" type="text" value="">
+        </div>
+        <div class="ym-hidden-inputs">
+        </div>
+        <input name="customerNumber" type="hidden" value="Оплата курса Энергия красоты в макияже">
+        <div class="ym-payment-btn-block ym-before-line ym-align-space-between">
+            <div class="ym-input-icon-rub ym-display-none">
+                <input name="sum" placeholder="0.00" class="ym-input ym-sum-input ym-required-input" type="number" step="any" value="15000">
+            </div>
+            <button data-text="Оплатить" class="ym-btn-pay ym-result-price"><span class="ym-text-crop">Оплатить</span></button>
+            <lineargradient id="paint0_linear" x1="57.3564" y1="0" x2="57.3564" y2="63" gradientunits="userSpaceOnUse"><stop stop-color="#0160D1"><stop offset="1" stop-color="#00479C"></stop></stop></lineargradient>
+            <lineargradient id="paint1_linear" x1="12.8472" y1="9.35889" x2="12.8472" y2="55.2393" gradientunits="userSpaceOnUse"><stop stop-color="#0160D1"><stop offset="1" stop-color="#00479C"></stop></stop></lineargradient>
+        </div>
+        <input name="shopId" type="hidden" value="957982">
+    </form>
+</div>
+
+<footer>
+    <div>
+        <p>Вяткина Вероника Андреевна</p>
+        <p>ИНН 280605583192</p>
+    </div>
+<a class="doc" href="https://veroniikamo.ru/offerta.pdf" target="_blank">Публичная офферта</a>
+<a class="doc" href="https://veroniikamo.ru/politika.pdf" target="_blank">Политика обработки персональных данных</a>
+
+    <div class="sours">
+        <a href="https://icons8.ru" target="_blank">icons8</a>
+        <span>Разработка сайта <a href="https://t.me/variarmo" target="_blank">Variarmo</a></span>
+    </div>
+
+</footer>
+<script src="./js/jquery-3.7.1.min.js"></script>
+<script src="./js/script.js"></script>
+</body>
+</html>
